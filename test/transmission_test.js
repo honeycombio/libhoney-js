60ca9cb8485479d1bcddecaab5c6561ff22b54dd
Chris Toshok <toshok@gmail.com>
2016-12-12 13:22:08 -0800

    add urljoin dep and use it to build up urls (#3)
    add urljoin dep and use it to build up the url for sending events.  keeps trailing slashes from being a probem.
    
    Add a mock on superagent-mocker so we can verify that the urljoin'ing works.

--- test/transmission_test.js
+++ test/transmission_test.js
@@ -1,8 +1,36 @@
 import assert from 'assert';
 import Transmission from '../lib/transmission';
 
+let superagent = require('superagent');
+let mock = require('superagent-mocker')(superagent);
+
 describe('transmission', function() {
 
+  it('should handle apiHosts with trailing slashes', function(done) {
+    let endpointHit = false;
+    mock.post('http://localhost:9999/1/events/test-transmission', function(req) {
+      endpointHit = true;
+      return {};
+    });
+
+    var transmission = new Transmission({
+      batchTimeTrigger: 0,
+      responseCallback: function(resp) {
+        assert.equal(true, endpointHit);
+        done();
+      }
+    });
+
+    transmission.sendEvent({
+      apiHost: "http://localhost:9999/",
+      writeKey: "123456789",
+      dataset: "test-transmission",
+      sampleRate: 1,
+      timestamp: new Date(),
+      postData: JSON.stringify({ a: 1, b: 2 })
+    });
+  });
+     
   it('should eventually send a single event (after the timeout)', function(done) {
     var transmission = new Transmission({
       batchTimeTrigger: 10,
@@ -12,9 +40,9 @@ describe('transmission', function() {
     });
 
     transmission.sendEvent({
-      postUrl: "http://localhost:9999",
+      apiHost: "http://localhost:9999",
       writeKey: "123456789",
-      dataSet: "test-transmission",
+      dataset: "test-transmission",
       sampleRate: 1,
       timestamp: new Date(),
       postData: JSON.stringify({ a: 1, b: 2 })
@@ -31,9 +59,9 @@ describe('transmission', function() {
 
     transmission._randomFn = function() { return 0.09 };
     transmission.sendEvent({
-      postUrl: "http://localhost:9999",
+      apiHost: "http://localhost:9999",
       writeKey: "123456789",
-      dataSet: "test-transmission",
+      dataset: "test-transmission",
       sampleRate: 10,
       timestamp: new Date(),
       postData: JSON.stringify({ a: 1, b: 2 })
@@ -42,18 +70,18 @@ describe('transmission', function() {
 
   it('should respect sample rate and drop the event', function(done) {
     var transmission = new Transmission({
-      batchTimeTrigger: 10,
+      batchTimeTrigger: 10
     });
 
-    transmission._randomFn = function() { return 0.11 };
+    transmission._randomFn = function() { return 0.11; };
     transmission._droppedCallback = function() {
       done();
     };
 
     transmission.sendEvent({
-      postUrl: "http://localhost:9999",
+      apiHost: "http://localhost:9999",
       writeKey: "123456789",
-      dataSet: "test-transmission",
+      dataset: "test-transmission",
       sampleRate: 10,
       timestamp: new Date(),
       postData: JSON.stringify({ a: 1, b: 2 })
