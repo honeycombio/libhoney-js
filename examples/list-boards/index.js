f183aa409a97dddc7bfc4d47d856c9787d8ad70e
Chris Toshok <toshok@hound.sh>
2018-07-15 13:26:11 -0700

    add a tiny example of the CRUD api that lists boards

---
+++ examples/list-boards/index.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+const Libhoney = require("libhoney");
+const process = require("process");
+
+const honey = new Libhoney({
+  apiHost: process.env.HONEYCOMB_API_HOST,
+  apiKey: process.env.HONEYCOMB_WRITEKEY
+});
+
+honey.boards
+  .list()
+  .then(boards =>
+    boards.forEach(b => {
+      console.log(`Name: ${b.name}`);
+      console.log(b.queries);
+    })
+  )
+  .catch(err => {
+    console.error(`got error ${err} :( :( :(`);
+  });
