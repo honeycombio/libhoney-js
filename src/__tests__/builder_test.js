d0b444db1e3c3e028f020b330733deb8f036c103
Ally Weir <aweir@twig-world.com>
2019-03-25 22:53:53 +0000

    Update builder tests to use Jest's expect syntax

--- src/__tests__/builder_test.js
+++ src/__tests__/builder_test.js
@@ -1,5 +1,4 @@
 /* eslint-env node, jest */
-import assert from "assert";
 import libhoney from "../libhoney";
 
 describe("libhoney builder", function() {
@@ -14,11 +13,11 @@ describe("libhoney builder", function() {
         }
       }
     );
-    assert.equal(5, b._fields.a);
-    assert.equal(undefined, b._fields.b);
+    expect(b._fields.a).toEqual(5);
+    expect(b._fields.b).toEqual(undefined);
     b = hny.newBuilder();
-    assert.equal(0, Object.getOwnPropertyNames(b._fields).length);
-    assert.equal(0, Object.getOwnPropertyNames(b._dyn_fields).length);
+    expect(Object.getOwnPropertyNames(b._fields)).toHaveLength(0);
+    expect(Object.getOwnPropertyNames(b._dyn_fields)).toHaveLength(0);
   });
 
   it("accepts dict-like arguments to .add()", function() {
@@ -28,14 +27,14 @@ describe("libhoney builder", function() {
     b = hny.newBuilder();
     b.add({ a: 5 });
     ev = b.newEvent();
-    assert.equal(5, ev.data.a);
+    expect(ev.data.a).toEqual(5);
 
     var map = new Map();
     map.set("a", 5);
     b = hny.newBuilder();
     b.add(map);
     ev = b.newEvent();
-    assert.equal(5, ev.data.a);
+    expect(ev.data.a).toEqual(5);
   });
 
   it("doesn't stringify object values", function() {
@@ -53,8 +52,8 @@ describe("libhoney builder", function() {
 
     builder.sendNow({ c: { d: 2 } });
 
-    assert.equal(transmission.events.length, 1);
-    assert.equal(transmission.events[0].postData, JSON.stringify(postData));
+    expect(transmission.events).toHaveLength(1);
+    expect(transmission.events[0].postData).toEqual(JSON.stringify(postData));
   });
 
   it("includes snapshot of global fields/dyn_fields", function() {
@@ -75,8 +74,8 @@ describe("libhoney builder", function() {
 
     builder.sendNow({ c: 3 });
 
-    assert.equal(transmission.events.length, 1);
-    assert.equal(transmission.events[0].postData, JSON.stringify(postData));
+    expect(transmission.events).toHaveLength(1);
+    expect(transmission.events[0].postData).toEqual(JSON.stringify(postData));
 
     // but if we create another builder, it should show up in the post data.
     postData = { a: 1, b: 2, c: 3 };
@@ -85,7 +84,7 @@ describe("libhoney builder", function() {
 
     builder.sendNow({ c: 3 });
 
-    assert.equal(transmission.events.length, 2);
-    assert.equal(transmission.events[1].postData, JSON.stringify(postData));
+    expect(transmission.events).toHaveLength(2);
+    expect(transmission.events[1].postData).toEqual(JSON.stringify(postData));
   });
 });
